3a28681beb32c2f6f1f5e593d3bf79e90788f0bd
Bzzmn <alvaro.acevedo.ing@gmail.com>
2024-09-13 23:25:03 -0300

    adittional testing and Makefile for deployment

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -27,6 +27,7 @@ contract HelperConfig is Script, CodeConstants {
         uint32 callbackGasLimit;
         uint256 subscriptionId;
         address link;
+        address account;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -61,7 +62,8 @@ contract HelperConfig is Script, CodeConstants {
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 subscriptionId: 8439194383223007265821783704491785940106436192315434957244348576496489888204,
                 callbackGasLimit: 500000,
-                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                account: 0x29c2d9b84A2D0A52A899E1E85b600a6aad7Cc950
             });
     }
 
@@ -89,7 +91,8 @@ contract HelperConfig is Script, CodeConstants {
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 0, // might have to fix this
             callbackGasLimit: 500000,
-            link: address(linkToken)
+            link: address(linkToken),
+            account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
         });
 
         return localNetworkConfig;
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -213,4 +213,12 @@ contract Raffle is VRFConsumerBaseV2Plus {
     ) external view returns (address) {
         return s_participants[indexOfPlayer];
     }
+
+    function getLastTimeStamp() external view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRecentWinner() external view returns (address) {
+        return s_recentWinner;
+    }
 }
